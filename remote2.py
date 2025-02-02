3d310bce2166249f0a62b63b6177ab74c5d41d0c
Ubaidillah30 <Maulidulula03@gmail.com>
2025-02-02 21:26:30 +0700

    menambah kata terima kasih

--- remote2.py
+++ remote2.py
@@ -4,3 +4,5 @@ if (angka >= 1 and angka <= 10):
     print(f"angka yang anda masukkan adalah: {angka}")
 else : 
     print("angka yang anda masukkan tidak valid!")
+    
+print("terima kasih sudah mencoba")
