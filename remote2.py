f3eff55bdb484c263697637671e1ebcaf798bc26
Ubaidillah30 <maulidulula03@gmail.com>
2025-02-02 21:13:36 +0700

    menambahkan file remote.py

---
+++ remote2.py
@@ -0,0 +1,6 @@
+angka = int(input("masukkan nilai dengan rentang 1-10: "))
+
+if (angka >= 1 and angka <= 10):
+    print(f"angka yang anda masukkan adalah: {angka}")
+else : 
+    print("angka yang anda masukkan tidak valid!")
